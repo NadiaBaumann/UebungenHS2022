cdfca572df4d194e09591c9179fcb2c453b7c5c3
NadiaBaumann <nadia.baumann@students.fhnw.ch>
2022-10-21 11:24:08 +0200

    uebung4

---
+++ uebung04/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Timer from "./Timer";
+function App() {
+    return (<>
+        <h1>Uebung 4: Erste Schritte mit React.js</h1>
+        <h2>Aufgabe 1:</h2>
+        <Timer countdown="50" /><hr/>
+
+
+</>); }
+export default App;
